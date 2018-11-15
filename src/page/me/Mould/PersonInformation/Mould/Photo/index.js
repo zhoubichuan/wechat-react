1363af1a643be3332e321a667c5943b14391327f
“huipaodejituigit config --global user.email 920210993@qq.comgit config --global user.name “huipaodejitui <920210993@qq.com>
2018-11-15 22:43:41 +0800

    '个人信息文件结构'

---
+++ src/page/me/Mould/PersonInformation/Mould/Photo/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Photo extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>头像</div>;
+  }
+}
--- src/page/me/Mould/PersonInformation/index.js
+++ src/page/me/Mould/PersonInformation/index.js
@@ -1,59 +1,75 @@
 import React, { Component } from "react";
 import Header from "../../../../base-page/Header";
+import { Link } from "react-router-dom";
 import "./index.less";
 
 class PersonInformation extends Component {
   constructor() {
     super();
+    this.state = {
+      showMould: false,
+      personData: [{ title: "头像", img: "", content: "", ico: "" }]
+    };
   }
   handleClick = () => {
     window.history.go(0);
   };
+  handleClick2 = e => {
+    this.setState({
+      showMould: true
+    });
+  };
   render() {
     return (
       <div component={"me-personinformation-component"}>
-        <Header>
-          <li onClick={this.handleClick}>我</li>
-          <li>个人信息</li>
-          <li />
-        </Header>
-        <div className={"person-information"}>
-          <ul>
-            <li className={"photo"}>
-              <p>头像</p>
-              <div className="right">
-                <img src="http://www.cdhdky.com/images/ttt.jpg" alt="" />
-                <i className="ico" />
-              </div>
-            </li>
-            <li className={"name"}>
-              <p>名字</p>
-              <div className="right">
-                <span>会跑的鸡腿</span>
-                <i className="ico" />
-              </div>
-            </li>
-            <li className={"weixin"}>
-              <p>微信号</p>
-              <div>abc159x</div>
-            </li>
-            <li className={"data-cord"}>
-              <p>我的二维码</p>
-              <div className="right">
-                <i className="code" />
-                <i className="ico" />
-              </div>
-            </li>
-            <li className={"more"}>
-              <p>更多</p>
-              <i className="ico" />
-            </li>
-            <li className={"address"}>
-              <p>我的地址</p>
-              <i className="ico" />
-            </li>
-          </ul>
-        </div>
+        {!this.state.showMould && (
+          <div>
+            <Header>
+              <li onClick={this.handleClick}>我</li>
+              <li>个人信息</li>
+              <li />
+            </Header>
+            <div className={"person-information"}>
+              <ul>
+                <li className={"photo"} onClick={this.handleClick2}>
+                  <Link to={"/Me/PersonInformation/Photo"}>
+                    <p>头像</p>
+                    <div className="right">
+                      <img src="http://www.cdhdky.com/images/ttt.jpg" alt="" />
+                      <i className="ico" />
+                    </div>
+                  </Link>
+                </li>
+                <li className={"name"}>
+                  <p>名字</p>
+                  <div className="right">
+                    <span>会跑的鸡腿</span>
+                    <i className="ico" />
+                  </div>
+                </li>
+                <li className={"weixin"}>
+                  <p>微信号</p>
+                  <div>abc159x</div>
+                </li>
+                <li className={"data-cord"}>
+                  <p>我的二维码</p>
+                  <div className="right">
+                    <i className="code" />
+                    <i className="ico" />
+                  </div>
+                </li>
+                <li className={"more"}>
+                  <p>更多</p>
+                  <i className="ico" />
+                </li>
+                <li className={"address"}>
+                  <p>我的地址</p>
+                  <i className="ico" />
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
